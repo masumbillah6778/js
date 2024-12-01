06d20798d53cf0b8139b26514662676db4be2e83
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2024-12-02 02:25:11 +0600

    Add files via upload

---
+++ dropdown-menu.js
@@ -0,0 +1,24 @@
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const select = document.querySelector(".select");
+  const caret = document.querySelector(".caret");
+  const menu = document.querySelector(".menu");
+  const options = document.querySelectorAll(".menu li");
+  const selected = document.querySelector(".selected");
+  select.addEventListener("click", () => {
+    caret.classList.toggle("caret-rotate");
+    menu.classList.toggle("menu-open");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      selected.classList.remove("placeholder");
+      selected.innerText = option.innerText;
+      caret.classList.remove("caret-rotate");
+      menu.classList.remove("menu-open");
+      options.forEach((option) => option.classList.remove("active"));
+      option.classList.add("active");
+    });
+  });
+});
