e056470b482392ecb56068ec809f9906f85454ac
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2025-11-25 20:19:40 +0600

    Create foating-action-cercle-button-v1.js

---
+++ foating-action-cercle-button-v1.js
@@ -0,0 +1,36 @@
+// Get all menu from document
+document.querySelectorAll('.triggerButton').forEach(OpenMenu);
+
+// Menu Open and Close function
+function OpenMenu(active) {
+  if(active.classList.contains('triggerButton') === true){
+    active.addEventListener('click', function (e) {
+      e.preventDefault();        
+
+      if (this.nextElementSibling.classList.contains('active') === true) {
+        // Close the clicked dropdown
+        this.parentElement.classList.remove('active');
+        this.nextElementSibling.classList.remove('active');
+
+      } else {
+        // Close the opend dropdown
+        closeMenu();
+        // add the open and active class(Opening the DropDown)
+        this.parentElement.classList.add('active');
+        this.nextElementSibling.classList.add('active');
+      }
+    });
+  }
+};
+
+// Close the openend Menu
+function closeMenu() { 
+  // remove the open and active class from other opened Moenu (Closing the opend Menu)
+  document.querySelectorAll('.radial').forEach(function (container) { 
+    container.classList.remove('active')
+  });
+
+  document.querySelectorAll('.radialMenu').forEach(function (menu) { 
+    menu.classList.remove('active');
+  });
+}
