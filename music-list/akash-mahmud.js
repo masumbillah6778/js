1c4a7338585c5a317394522cee4d89c9d71d9ae8
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-01-02 01:23:12 +0600

    Add music data for Akash Mahmud

---
+++ music-list/akash-mahmud.js
@@ -0,0 +1,49 @@
+'use strict';
+const musicData = [
+  {
+    backgroundImage: "https://masumbillah6778.github.io/images/music-thumbnail-10.jpg",
+    posterUrl: "https://masumbillah6778.github.io/images/music-thumbnail-10.jpg",
+    title: "1. Piya Re",
+    album: "No Spirit",
+    year: 2025,
+    artist: "No Spirit x Tonion",
+    musicPath: "https://github.com/masumbillah6778/best-of-zubeen-garg/raw/main/piya-re-piya-re.mp3",
+  },
+  {
+    backgroundImage: "https://masumbillah6778.github.io/images/singer/akash-mahmud.jpg",
+    posterUrl: "https://masumbillah6778.github.io/images/singer/akash-mahmud.jpg",
+    title: "2. We Are Going To Be Ok (Master)",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit x Jhove",
+    musicPath: "https://github.com/masumbillah6778/best-of-zubeen-garg/raw/main/piya-re-piya-re.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-3.jpg",
+    posterUrl: "https://masumbillah6778.github.io/images/singer/akash-mahmud.jpg",
+    title: "Winter Meadow",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit x  juniorodeo",
+    musicPath: "./assets/music/music-3.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-4.jpg",
+    posterUrl: "https://masumbillah6778.github.io/images/singer/akash-mahmud.jpg",
+    title: "From Where We Started",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit",
+    musicPath: "./assets/music/music-4.mp3",
+  },
+  {
+    backgroundImage: "./assets/images/poster-5.jpg",
+    posterUrl: "https://masumbillah6778.github.io/images/singer/akash-mahmud.jpg",
+    title: "Where I Found You",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit",
+    musicPath: "./assets/music/music-5.mp3",
+  },
+];
+
