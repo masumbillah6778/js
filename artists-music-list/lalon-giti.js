a93e69a3dd646219782b93bfda84d3a404ae05ba
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-01-29 21:53:36 +0600

    Add files via upload

---
+++ artists-music-list/lalon-giti.js
@@ -0,0 +1,21 @@
+'use strict';
+const musicData = [
+  {
+    backgroundImage: "https://masumbillah6778.github.io/images/lalon.png",
+    posterUrl: "https://masumbillah6778.github.io/images/lalon.png",
+    title: "1. Piya Re",
+    album: "No Spirit",
+    year: 2025,
+    artist: "No Spirit x Tonion",
+    musicPath: "https://github.com/masumbillah6778bd/lalon-giti/raw/main/ami-opar-hoye-bose-asi.mp3",
+  },
+  {
+    backgroundImage: "https://masumbillah6778.github.io/images/lalon.png",
+    posterUrl: "https://masumbillah6778.github.io/images/lalon.png",
+    title: "Where I Found You",
+    album: "No Spirit",
+    year: 2022,
+    artist: "No Spirit",
+    musicPath: "./assets/music/music-5.mp3",
+  },
+];
