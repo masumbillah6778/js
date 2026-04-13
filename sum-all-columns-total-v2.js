fcb89a88567fcbd3ee2ab3097fc84e806a276970
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-04-13 22:18:29 +0600

    Create sum-all-columns-total-v2.js

---
+++ sum-all-columns-total-v2.js
@@ -0,0 +1,31 @@
+// Source - https://stackoverflow.com/a/71971397
+// Posted by Barmar, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-13, License - CC BY-SA 4.0
+
+var final = 0
+var tbody = document.querySelector("tbody");
+var howManyCols = tbody.rows[0].cells.length;
+var totalRow = tbody.rows[tbody.rows.length - 1];
+for (var j = 1; j < howManyCols; j++) {
+  final = computeTableColumnTotal(j);
+  totalRow.cells[j].innerText = final;
+}
+
+function computeTableColumnTotal(colNumber) {
+
+  var result = 0;
+
+  try {
+    var tableBody = document.querySelector("tbody");
+    var howManyRows = tableBody.rows.length;
+
+    for (var i = 1; i < (howManyRows - 1); i++) {
+      var thisNumber = parseInt(tableBody.rows[i].cells[colNumber].childNodes.item(0).data);
+
+      if (!isNaN(thisNumber))
+        result += thisNumber;
+    }
+  } finally {
+    return result;
+  }
+}
