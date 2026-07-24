9582aafca36da3d67cd58a32aeb23c5a5a2ec2bf
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-07-25 03:14:26 +0600

    Fix spacing in empty row message

--- data-local-storage.js
+++ data-local-storage.js
@@ -1,4 +1,4 @@
-var emptyRow = "<tr><td colspan='10' class='text-center'> কোনো তথ্য এন্ট্রি নেই! 😡 </td></tr>";
+var emptyRow = "<tr><td colspan='10' class='text-center'> কোনো তথ্য এন্ট্রি নেই ! 😡 </td></tr>";
     $(document).ready(function () {
       loadDataFromLocal();
       $('#tblData').on('click', '.btn-edit', function () {
