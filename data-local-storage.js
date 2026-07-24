03b9685a496bacbc17cd7fe146a0a3e1a3e3d7ea
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2026-07-24 22:45:26 +0600

    Implement table filtering functionality
    Added a table filtering function to filter rows based on user input.

--- data-local-storage.js
+++ data-local-storage.js
@@ -180,3 +180,31 @@ var emptyRow = "<tr><td colspan='6' class='text-center'> No Records Available</t
       localStorage.setItem('localData', JSON.stringify(localArray));
       loadDataFromLocal();
     }
+
+
+
+
+
+
+/*==================================================== Table Filter ====================================================*/
+
+function filterTable() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tblData");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td");
+    for (var j = 0; j < td.length; j++) {
+      txtValue = td[j].textContent || td[j].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        break;
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
